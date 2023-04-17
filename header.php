06ad75517ef3bcb81b013fe18a16ef928b975e07
magalyyanezg <a3521110208@alumno.uttehuacan.edu.mx>
2023-04-16 21:37:12 -0600

    segunda versión

--- header.php
+++ header.php
@@ -7,6 +7,6 @@
     <link rel="stylesheet" href="./public/bootstrap5/bootstrap.min.css">
     <link rel="stylesheet" href="./public/fontawesome-free-6.1.1-web/css/all.css">
     <link rel="stylesheet" href="./public/css/estilo.css">
-    <title>Crud mongo con php</title>
+    <title>Crud mongo con php | AMYG</title>
 </head>
 <body>
